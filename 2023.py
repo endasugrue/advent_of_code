ca316fb92970e164597b50b8bf9fc8e96f4e67ea
Enda Sugrue <me@endasugrue.com>
2023-12-06 09:15:22 -0500

    Day 3 complete - Finally git add .git add .

--- 2023.py
+++ 2023.py
@@ -4,6 +4,8 @@ import logging
 from script_secrets import Secrets
 import re
 from collections import defaultdict
+from pprint import pprint, pformat
+from functools import reduce
 
 setup_logging('advent_of_code')
 logger = logging.getLogger('advent_of_code')
@@ -17,6 +19,7 @@ def get_input_for_day(day:int) -> str:
     if req.status_code == 200:
         return req.text
     else:
+        logger.error(req.text)
         raise ValueError(f"Unable to get input for {day=}")
 
 def day_1_1(input:str) -> int:
@@ -77,27 +80,141 @@ def day_2_2(input:str) -> int:
         min_cubes_powers.append((min_cubes["red"]*min_cubes["blue"]*min_cubes["green"]))
     return sum(min_cubes_powers)
 
+def day_3_1(input:str) -> int:
+    input_as_list = input.strip().split("\n")
+    symbols_in_input_file = "*#-+@%&=$/"
+    line_symbols_data = {}
+    line_number_data = {}
+    numbers_to_be_added = []
+    for index,row in enumerate(input_as_list):
+        line_symbols_data[index] = []
+        line_number_data[index] = []
+        numbers = re.finditer(r"\d+", row)
+        for num in numbers:
+            value = num.group(0)
+            min_adj_index = num.start(0) - 1 if (num.start(0) - 1) > 0 else 0
+            max_adj_index = num.end(0) + 1
+            logger.info(f"{index=}, {value=}, {min_adj_index=}, {max_adj_index=}")
+            # Scan row above
+            if index != 0:
+                row_above = input_as_list[index - 1]
+                area_to_check_for_symbol_above = row_above[min_adj_index:max_adj_index]
+                logger.info(f"{area_to_check_for_symbol_above=}")
+                for c in area_to_check_for_symbol_above:
+                    if c in symbols_in_input_file:
+                        logger.info(f"Symbol {c} found in {area_to_check_for_symbol_above}")
+                        numbers_to_be_added.append(value)
+
+            # Current row
+            area_to_check_for_symbol_current = row[min_adj_index:max_adj_index]
+            logger.info(f"{area_to_check_for_symbol_current}")
+            for c in area_to_check_for_symbol_current:
+                if c in symbols_in_input_file:
+                    logger.info(f"Symbol {c} found in {area_to_check_for_symbol_current}")
+                    numbers_to_be_added.append(value)
+
+            # Row below
+            if index < len(input_as_list)-1:
+                row_below = input_as_list[index + 1]
+                area_to_check_for_symbol_below = row_below[min_adj_index:max_adj_index]
+                logger.info(f"{area_to_check_for_symbol_below=}")
+                for c in area_to_check_for_symbol_below:
+                    if c in symbols_in_input_file:
+                        logger.info(f"Symbol {c} found in {area_to_check_for_symbol_below}")
+                        numbers_to_be_added.append(value)
+            
+    return sum(map(int,numbers_to_be_added))
+
+def day_3_2(input:str) -> int:
+    
+    input_as_list = input.strip().split("\n")
+    gear_data = []
+    number_data = []
+    numbers_to_be_added = []
+    for index,row in enumerate(input_as_list):
+        numbers = re.finditer(r"\d+", row)
+        for num in numbers:
+            value = num.group(0)
+            start_index = num.start(0)-1 if num.start(0) -1 > 0 else 0
+            fin_index = num.end(0)
+            number_data.append({"index": index, "value":value, "start_index": start_index, "fin_index": fin_index})
+            print(row[start_index:fin_index], value, index)
+        gears = re.finditer(r"\*", row)
+        for gear in gears:
+            gear_data.append({"line_index": index, "gear_index": gear.start(0)})
+    
+    for g in gear_data:
+        numbers_to_be_multiplied = []
+        line_index = g.get("line_index")
+        gear_index = g.get("gear_index")
+        
+        #Line above
+        numbers_on_line_above = [n for n in number_data if n.get("index") == line_index -1]
+        numbers_on_current_line = [n for n in number_data if n.get("index") == line_index]
+        numbers_on_line_below = [n for n in number_data if n.get("index") == line_index+1]
+
+        for num in numbers_on_line_above:
+            if (gear_index >= num.get("start_index")) and (gear_index <= num.get("fin_index")):
+                logger.info(f"Number found adjacent on line above {num}, {gear_index=}")
+                numbers_to_be_multiplied.append(num.get("value"))
+        
+        for num in numbers_on_current_line:
+            if (gear_index >= num.get("start_index")) and (gear_index <= num.get("fin_index")):
+                logger.info(f"Number found adjacent on current line {num}, {gear_index=}")
+                numbers_to_be_multiplied.append(num.get("value"))
+
+        for num in numbers_on_line_below:
+            if (gear_index >= num.get("start_index")) and (gear_index <= num.get("fin_index")):
+                logger.info(f"Number found adjacent on line below {num}, {gear_index=}")
+                numbers_to_be_multiplied.append(num.get("value"))
+        
+        if len(numbers_to_be_multiplied) == 2:
+            numbers_to_be_added.append(int(numbers_to_be_multiplied[0])*int(numbers_to_be_multiplied[1]))
+
+    
+    return sum(numbers_to_be_added)
 
 
 
 if __name__ == "__main__":
     
-    # Day 1
-    logger = logging.getLogger('advent_of_code.day_1')
-    day_1_input = get_input_for_day(1)
+    # # Day 1
+    # logger = logging.getLogger('advent_of_code.day_1')
+    # day_1_input = get_input_for_day(1)
     
-    day_1_1_answer = day_1_1(day_1_input)
-    logger.info(f"{day_1_1_answer=}")
+    # day_1_1_answer = day_1_1(day_1_input)
+    # logger.info(f"{day_1_1_answer=}")
 
-    day_1_2_answer = day_1_2(day_1_input)
-    logger.info(f"{day_1_2_answer=}")
+    # day_1_2_answer = day_1_2(day_1_input)
+    # logger.info(f"{day_1_2_answer=}")
 
-    # Day 2
-    logger = logging.getLogger('advent_of_code.day_2')
-    day_2_input = get_input_for_day(2)
+    # # Day 2
+    # logger = logging.getLogger('advent_of_code.day_2')
+    # day_2_input = get_input_for_day(2)
 
-    day_2_2_answer = day_2_1(day_2_input)
-    logger.info(f"{day_2_2_answer=}")
+    # day_2_1_answer = day_2_1(day_2_input)
+    # logger.info(f"{day_2_1_answer=}")
 
-    day_2_2_answer = day_2_2(day_2_input)
-    logger.info(f"{day_2_2_answer=}")
+    # day_2_2_answer = day_2_2(day_2_input)
+    # logger.info(f"{day_2_2_answer=}")
+
+    # Day 3
+    logger = logging.getLogger('advent_of_code.day_3')
+    day_3_input = get_input_for_day(3)
+#     day_3_input = """467..114..
+# ...*......
+# ..35..633.
+# ......#...
+# 617*......
+# .....+.58.
+# ..592.....
+# ......755.
+# ...$.*....
+# .664.598.."""
+
+
+    day_3_1_answer = day_3_1(day_3_input)
+    logger.info(f"{day_3_1_answer=}")
+
+    # day_3_2_answer = day_3_2(day_3_input)
+    # logger.info(f"{day_3_2_answer=}")
--- script_secrets.py
+++ script_secrets.py
@@ -10,5 +10,21 @@ class Secrets:
     aoc_session_token = str
     
     def __init__(self) -> None:
-        self.aoc_session_token = "<SESSION TOKEN FROM ADVENT OF CODE WEBSITE COOKIES>"
+        self.aoc_session_token = get_secret("aoc_session_token").get('password')
 
+
+def get_secret(bw_name):
+    logger.debug(f'Get secret {bw_name}')
+    try:
+        requests.post("http://127.0.0.1:8087/sync")
+        secret_details = requests.get(f'http://127.0.0.1:8087/object/item/{bw_name}')
+        if secret_details.status_code == 200 and secret_details.json().get("success"):
+            secret_details_json = secret_details.json()
+            uname = secret_details_json["data"]["login"]["username"]
+            pw = secret_details_json["data"]["login"]["password"]
+            uname_password = {"username": uname, "password":pw}
+            return uname_password
+        else:
+            logger.error(f"Not successful getting secret {bw_name} - {secret_details}")
+    except Exception as e:
+        logger.error(f"Error getting secret {bw_name} - {e}")
